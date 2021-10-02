d00fd1aa145af4f543d002d2da28cfbb27e3f8b5
guswns1659 <guswns1659@gmail.com>
2021-10-02 20:25:02 +0900

    feat: Add KitchenService

---
+++ src/main/java/com/titanic/springreactive/hackingtutorial/KitchenService.java
@@ -0,0 +1,26 @@
+package com.titanic.springreactive.hackingtutorial;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class KitchenService {
+
+    Flux<Dish> getDishes() {
+        return Flux.<Dish> generate(sink -> sink.next(randomDish())).delayElements(Duration.ofMillis(250));
+    }
+
+    private List<Dish> menu = Arrays.asList(
+        new Dish("Sesame"),
+        new Dish("Lo mein noodles"),
+        new Dish("sweet")
+    );
+
+    private Dish randomDish() {
+        return menu.get(new Random().nextInt(menu.size()));
+    }
+}
