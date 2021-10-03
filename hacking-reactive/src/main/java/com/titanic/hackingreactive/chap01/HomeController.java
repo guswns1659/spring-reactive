e5bfa4a4983563dced191cfca6a40b52d25e6462
guswns1659 <guswns1659@gmail.com>
2021-10-03 15:03:20 +0900

    feat: Add home page rendering function

--- hacking-reactive/src/main/java/com/titanic/hackingreactive/chap01/HomeController.java
+++ hacking-reactive/src/main/java/com/titanic/hackingreactive/chap01/HomeController.java
@@ -1,14 +1,26 @@
 package com.titanic.hackingreactive.chap01;
 
+import com.titanic.hackingreactive.chap02.Cart;
+import com.titanic.hackingreactive.chap02.CartRepository;
+import com.titanic.hackingreactive.chap02.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
+
     @GetMapping
-    Mono<String> home() {
-        return Mono.just("home");
+    Mono<Rendering> home() {
+        return Mono.just(Rendering.view("home.html")
+            .modelAttribute("items", itemRepository.findAll())
+            .modelAttribute("cart", cartRepository.findById("My Cart").defaultIfEmpty(new Cart("My Cart")))
+            .build());
     }
 }
