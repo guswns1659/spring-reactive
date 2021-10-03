40eaaba14015c3cc7cfc3dda0790f77a3c35b426
guswns1659 <guswns1659@gmail.com>
2021-10-03 12:50:43 +0900

    feat: Create hacking-reactive sub module

---
+++ hacking-reactive/src/main/java/com/titanic/hackingreactive/HackingReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.titanic.hackingreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackingReactiveApplication.class, args);
+    }
+
+}
---
+++ hacking-reactive/src/test/java/com/titanic/hackingreactive/HackingReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titanic.hackingreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
