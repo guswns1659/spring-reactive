2b3560dccaec4ac53abbf444af06159b346feb64
guswns1659 <guswns1659@gmail.com>
2021-10-03 15:39:41 +0900

    feat: Add CartService

---
+++ hacking-reactive/src/main/java/com/titanic/hackingreactive/chap02/CartService.java
@@ -0,0 +1,32 @@
+package com.titanic.hackingreactive.chap02;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
+
+    public Mono<Cart> addToCart(String cartId, String id) {
+        return cartRepository.findById("My Cart")
+            .defaultIfEmpty(new Cart("My Cart"))
+            .flatMap(cart -> cart.getCartItems().stream()
+                .filter(cartItem -> cartItem.getItem().getId().equals(id))
+                .findAny()
+                .map(cartItem -> {
+                    cartItem.increment();
+                    return Mono.just(cart);
+                })
+                .orElseGet(() -> itemRepository.findById(id)
+                    .map(CartItem::new)
+                    .map(cartItem -> {
+                        cart.getCartItems().add(cartItem);
+                        return cart;
+                    })))
+            .flatMap(cartRepository::save);
+    }
+}
