bb1887311a574d9da73ec698d016bae32b015c18
guswns1659 <guswns1659@gmail.com>
2021-10-05 13:36:28 +0900

    chore: Remove BlockHound in application

--- hacking-reactive/src/main/java/com/titanic/hackingreactive/HackingReactiveApplication.java
+++ hacking-reactive/src/main/java/com/titanic/hackingreactive/HackingReactiveApplication.java
@@ -2,18 +2,16 @@ package com.titanic.hackingreactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.thymeleaf.TemplateEngine;
-import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class HackingReactiveApplication {
 
     public static void main(String[] args) {
         // TODO(jack.comeback) : blockingHound는 테스트 단위에서만 활용되도록 한다.
-        BlockHound.builder()
-            .allowBlockingCallsInside(
-                TemplateEngine.class.getCanonicalName(), "process"
-            ).install();
+//        BlockHound.builder()
+//            .allowBlockingCallsInside(
+//                TemplateEngine.class.getCanonicalName(), "process"
+//            ).install();
 
         SpringApplication.run(HackingReactiveApplication.class, args);
     }
